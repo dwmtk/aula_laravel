da57a4aa719ba25ec6cd27b157b65ba20724f76e
motoki <yuki.momayu@gmail.com>
2020-06-06 14:13:34 +0900

    2020/06/06 14:13

--- resources/views/mycarinfo.blade.php
+++ resources/views/mycarinfo.blade.php
@@ -30,18 +30,24 @@
                             <div class="d-block pt-3 pb-0" style="background: #EDEFF2; border-radius: 8px;">
                             <div class="mx-auto" style="max-width:250px;">
                             <table class="table table-sm table-borderless">
-                            <tr><td scope="row" style="width:85px;">メーカー：</td><td>{{ $mycar->car_maker }}</td></tr>
+                            <tr><td scope="row" style="width:96px;">メーカー：</td><td>{{ $mycar->car_maker }}</td></tr>
                             <tr><td scope="row">車名：</td><td>{{ $mycar->car_name }}</td></tr>
                             <tr><td scope="row">年式：</td><td>{{ $mycar->car_age_start }}～{{ $mycar->car_age_end }}</td></tr>
                             <tr><td scope="row">ナンバー：</td><td>{{ $mycar->car_number }}</td></tr>
                             <tr><td scope="row">車色：</td><td>{{ $mycar->car_color }}</td></tr>
-
+                            <tr><td scope="row">最終洗車日：</td><td>
+                            @if( !is_null( $mycar->order_date ))
+                                {{date('Y年m月d日',  strtotime( $mycar->order_date )) }}
+                            @else
+                                洗車履歴はありません
+                            @endif
+                            </td></tr>
                             <tr><td colspan="2">
 
                             <form class="d-flex text-center mx-auto" style="max-width:250px;" method="POST" action="{{ url('/mycardelete') }}" onSubmit="return dialog('マイカーを解除しますか？')">
                             @csrf
                                 <input type="hidden" name="mycar_id" value="{{ $mycar->mycar_id }}">
-                                <input class="btn btn-sm btn-primary btn-lg flex-fill p-2" type="submit" value="解除する">
+                                <input class="btn btn-sm btn-secondary btn-lg flex-fill p-2" type="submit" value="解除する">
                             </form>
                             </td></tr>
                             </table>
