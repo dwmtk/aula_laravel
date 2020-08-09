e62a9ee03969eb1498830a5df459fa382815bfc2
MOTOKI SAKASHITA <yuki.momayu@gmail.com>
2020-08-09 13:00:15 +0900

    2020/08/09 13:00

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-// Route::get('/welcome2', function () {
-//     return view('welcome2');
-// });
+
 Route::get('/error', function () {
     return view('error');
 });
